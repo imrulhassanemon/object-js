0c0147621cc521c36ea96160ca3da1adfeef03d0
imrulhassanemon <imrulhassan989@gmail.com>
2024-09-07 03:47:12 +0600

    practicing object

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+    motherBoard: "ASUS",
+    price: 120000,
+    processor: "interl",
+    ssd : 512,
+    monitor: "sansung"
+}
+
+const k  = Object.keys(computer)
+console.log(k)
